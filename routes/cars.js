3ffa2175c82c6bcefaf7046db307edcb2c97c93c
Matthew Pourroy <pourroy.matt@gmail.com>
2022-06-25 21:09:23 -0400

    change css index

---
+++ routes/cars.js
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import { isLoggedIn } from "../middleware/middleware"
+
+const router = Router()
+
+router.get('/', function (req, res) {
+  res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
+})
+
+export {
+  router
+}
